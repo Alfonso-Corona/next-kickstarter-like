d5385b2fa3b1df7758129070ad42fb2f04759c63
Alfonso Corona <coronaalfonsor@gmail.com>
2025-08-06 18:52:52 -0600

    feat(): nextauth configured to connect with github session

---
+++ auth.ts
@@ -0,0 +1,9 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+
+export const { auth, signIn, signOut, handlers } = NextAuth({
+  providers: [GitHub({
+    clientId: process.env.AUTH_GITHUB_CLIENT_ID!,
+    clientSecret: process.env.AUTH_GITHUB_CLIENT_SECRET!,
+  })],
+});
