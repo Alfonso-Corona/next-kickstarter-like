1a7727f0fe5aec6215bd610af8f5197c08505f24
Alfonso Corona <coronaalfonsor@gmail.com>
2025-08-07 18:39:28 -0600

    feat(): creating startup cards navigations

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,50 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
-export default async function Home({searchParams}: {searchParams: Promise<{query?:string}>}) {
-
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
   const query = (await searchParams).query;
 
-  return <>
-  <section className="pink_container bg-pink-500 pattern">
-    <h2 className="heading">Pitch Your Startup, <br/> Connect With Entrepreneurs</h2>
-    <p className="sub-heading !max-w-3xl">Submit ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
-    <SearchForm query={query} />
-  </section>
-  </>;
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 150,
+      author: {_id: 1, name: "John Doe"},
+      _id: 1,
+      description: "Innovative AI startup",
+      image: "https://images.unsplash.com/photo-1754275047961-e2ed3624fda7?q=80&w=1548&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      category: "AI",
+      title: "AI Innovations"
+    }
+  ]
+
+  return (
+    <>
+      <section className="pink_container bg-pink-500 pattern">
+        <h2 className="heading">
+          Pitch Your Startup, <br /> Connect With Entrepreneurs
+        </h2>
+        <p className="sub-heading !max-w-3xl">
+          Submit ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+        <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (posts.map((post: StartupCardType) => (
+            <StartupCard post={post} key={post?._id}/>
+            ))): (
+            <p className="text-center text-gray-500">No startups found.</p>
+          )}
+        </ul>
+      </section>
+    </>
+  );
 }
---
+++ components/StartupCard.tsx
@@ -0,0 +1,49 @@
+import { formatDate } from "@/lib/utils";
+import { EyeIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const StartupCard = ({ post }: { post: StartupCardType }) => {
+
+  const {_createdAt, author: { _id: authorId, name }, views, title, category, _id, image, description} = post;
+
+  return (
+    <li className="startup-card group shadow-xl hover:border-pink-500 hover:shadow-2xl hover:bg-pink-200">
+      <div className="flex-between">
+        <p className="startup-card_date">{formatDate(_createdAt)}</p>
+        <div className="flex gap-1.5">
+          <EyeIcon className="size-6 text-pink-400" />
+          <span className="text-16-medium">{views}</span>
+        </div>
+      </div>
+
+      <div className="flex-between mt-5 gap-5">
+        <div className="flex-1">
+          <Link href={`/user/${authorId}`}>
+            <p className="text-16-medium line-clamp-1">{name}</p>
+          </Link>
+          <Link href={`/startup/${_id}`}>
+            <p className="text-26-semibold line-clamp-1">{title}</p>
+          </Link>
+        </div>
+        <Link href={`/user/${authorId}`}>
+          <Image width={48} height={48} src="https://placehold.co/48x48" alt={title} className="card-image" />
+        </Link>
+      </div>
+      <Link href={`/startup/${_id}`}>
+        <p className="startup-card_description">{description}</p>
+        <img src={image} alt={title} className="startup-card_img" />
+      </Link>
+      <div className="flex-between gap-3 mt-5">
+        <Link href={`/?query=${category.toLowerCase()}`}>
+          <p className="text-16-medium">{category}</p>
+        </Link>
+        <Button className="startup-card_btn" asChild>
+          <Link href={`/startup/${_id}`}>Details</Link>
+        </Button>
+      </div>
+    </li>
+  );
+};
+export default StartupCard;
